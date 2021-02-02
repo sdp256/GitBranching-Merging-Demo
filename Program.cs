7d1cc518d2e6c843279b1b8024675530b2e7a2c8
sdp256 <steevnpairy@gmail.com>
2021-02-01 19:40:50 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitbranchingdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
