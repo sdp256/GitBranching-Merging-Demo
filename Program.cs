2c0e4a812848a11e7dc6700b950b73199cedbf4e
sdp256 <steevnpairy@gmail.com>
2021-02-01 19:44:17 -0600

    created a feature branch and added 1 feature

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace gitbranchingdemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("more changes")
         }
     }
 }
